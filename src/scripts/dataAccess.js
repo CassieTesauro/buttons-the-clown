53950370e2cbbe95c282ba21a72476154d0df2fc
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-15 21:06:35 -0500

    added sendRequest() definition in dataAccess.js

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -19,3 +19,20 @@ export const fetchRequests = () => {  //5.
 export const getRequests = () => {  //13. getter function definition
     return applicationState.requests.map(request => ({...request}))  
 }
+
+
+export const sendRequest = (userServiceRequest) => { //26.
+    const fetchOptions = {  //27.
+        method: "POST",  //28.
+        headers: {
+            "Content-Type": "application/json"  //29.
+        },
+        body: JSON.stringify(userServiceRequest)  //30.
+    }
+    return fetch(`${API}/requests`, fetchOptions)  //31.
+        .then(response => response.json())  //32.
+        .then(() => {  //33.
+
+        })
+}
+
