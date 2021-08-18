5a25c20a72755f6002849988f2d1e9818816be4f
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-18 09:50:36 -0500

    added css styling

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -10,16 +10,16 @@ mainContainer.addEventListener("click", click => {
     }
 })
 
-
-
 export const Requests = () => { //16.
     const requests = getRequests() //17.
+    
     const requestListElement = (request) => { //36. deny button added
         return `<li>
         ${request.parentName}
         <button class="request__delete" id="request--${request.id}">Deny</button> 
         </li>`
         }  
+
     let html = `
         <ul>
             ${requests.map(requestListElement).join("")}
